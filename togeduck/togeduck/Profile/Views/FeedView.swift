7d26a937255a52e7f860097f8740659f8942b878
seongmin221 <seongmin221@naver.com>
2022-04-12 12:58:00 +0900

    feat: refactor feed

--- togeduck/togeduck/Profile/Views/FeedView.swift
+++ togeduck/togeduck/Profile/Views/FeedView.swift
@@ -8,51 +8,57 @@
 import SwiftUI
 
 struct FeedView: View {
+//    @State var isFavorite: Bool = false
     var body: some View {
         ScrollView(showsIndicators: false){
             VStack(spacing: 8){
                 ForEach(0 ..< 3){ index in
-                    VStack(alignment: .leading, spacing: 4) {
-                        HStack(spacing: 10) {
-                            Image("profileImage1")
+                    ScrollView{
+                        VStack(alignment: .leading, spacing: 4) {
+                            HStack(spacing: 10) {
+                                Image("profileImage1")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40)
+                                    .clipShape(Circle())
+                                    .overlay(Circle().stroke(Color.Gray200, lineWidth: 1))
+                                VStack(alignment: .leading, spacing: 2) {
+                                    Text("스키피")
+                                        .font(.system(size: 16, weight: .bold))
+                                    HStack(spacing: 2) {
+                                        Text("1시간 전")
+                                            .font(.system(size: 14, weight: .medium))
+                                            .foregroundColor(.Gray500)
+                                        Text("•")
+                                            .font(.system(size: 14, weight: .medium))
+                                            .foregroundColor(.Gray500)
+                                        Text("3km이내")
+                                            .font(.system(size: 14, weight: .medium))
+                                            .foregroundColor(.Gray500)
+                                    }//HStack
+                                }//VStack
+                            }//Hstack
+                            Image("feed_image_sample")
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-                                .frame(width: 40)
-                                .clipShape(Circle())
-                                .overlay(Circle().stroke(Color.Gray200, lineWidth: 1))
-                            VStack(alignment: .leading, spacing: 2) {
-                                Text("스키피")
-                                    .font(.system(size: 16, weight: .bold))
-                                HStack(spacing: 2) {
-                                    Text("1시간 전")
-                                        .font(.system(size: 14, weight: .medium))
-                                        .foregroundColor(.Gray500)
-                                    Text("•")
-                                        .font(.system(size: 14, weight: .medium))
-                                        .foregroundColor(.Gray500)
-                                    Text("3km이내")
-                                        .font(.system(size: 14, weight: .medium))
-                                        .foregroundColor(.Gray500)
-                                }//HStack
-                            }//VStack
-                        }//Hstack
-                        Image("feed_image_sample")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(12)
-                        Text("토트넘 4:0 아스톤빌라")
-                            .font(.system(size: 20, weight: .bold))
+                                .cornerRadius(12)
+                            VStack (alignment: .leading){
+                                HStack {
+                                    Text("토트넘 4:0 아스톤빌라")
+                                        .font(.system(size: 16, weight: .bold))
+                                }
+                                .padding(.top, 12)
+                                Text("아자자자~ 아자자자 아자자 아자자자 아자자 아자자 아자 두 줄 텍스트")
+                                    .font(.system(size: 14, weight: .regular))
+                                    .foregroundColor(.Gray600)
+                                    .padding(.top, -5)
+                                    .lineLimit(1)
+                            }
                             .padding(.horizontal, 8)
-                            .padding(.top, 12)
-                        Text("아자자자~ 아자자자 아자자 아자자자 아자자 아자자 아자 두 줄 텍스트")
-                            .font(.system(size: 17, weight: .regular))
-                            .foregroundColor(.Gray600)
-                            .lineSpacing(10)
-                            .padding(.horizontal, 8)
-                            .padding(.top, 2)
-                            .lineLimit(1)
-                        Spacer()
-                    }//VStack
+                        }
+                        Divider()
+                            .padding([.top, .bottom], 10)
+                    }
                 }//ForEach
             }//VStack
         }//ScrollView
@@ -68,6 +74,6 @@ extension Color {
 
 struct FeedView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FeedView()
     }
 }
